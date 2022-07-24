46fe74586a081bd7d57b0c85475ddacd6b62babb
Anas Hafsi <anashafsii@gmail.com>
2022-07-24 14:22:04 +0200

    Adding Header

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CodeFactor](https://www.codefactor.io/repository/github/anashafsi/awwwards_web_1/badge)](https://www.codefactor.io/repository/github/anashafsi/awwwards_web_1)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className='container'>
-            <div className='row v-center space-between'>
-                <div className='logo'>
-                    <a href='/'>Agency</a>
-                </div>
-                <div className="nav">
-                    <span></span>
-                    <span></span>
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="row v-center space-between">
+                    <div className="logo">
+                        <a href="/">AGENCY</a>
+                    </div>
+                    <div className="nav">
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
-}
+    );
+};
 export default Header;
