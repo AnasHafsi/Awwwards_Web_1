a3474e640edf36081789ec7a21c8fefc1b92f122
Anas Hafsi <anashafsii@gmail.com>
2022-07-24 14:22:41 +0200

    Adding Banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Banner from "./components/banner";
+import Cases from "./components/cases";
 import Header from "./components/header";
 import "./styles/App.scss";
 
 function App() {
-  return (
-    <div className='App'>
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Banner />
+            <Cases />
+        </div>
+    );
 }
 
 export default App;
